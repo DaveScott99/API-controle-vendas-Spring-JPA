37c016f6502699eb09d5e6b6343253a4d82e38aa
DaviSantos <davicalixto2077@gmail.com>
2022-10-19 15:23:06 -0300

    Project created

---
+++ src/main/java/com/astro/apiwebService/ApiWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.astro.apiwebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astro/apiwebService/ApiWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.apiwebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
