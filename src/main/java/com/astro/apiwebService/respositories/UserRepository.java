2edc5634cc4c5b0245a3fb340d2149ae6665ba98
DaviSantos <davicalixto2077@gmail.com>
2022-10-20 12:58:51 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/astro/apiwebService/respositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.astro.apiwebService.respositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.astro.apiwebService.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
