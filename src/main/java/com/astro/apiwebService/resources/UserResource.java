eec4d2d58903c5d265d90177873a62d15cf68205
DaviSantos <davicalixto2077@gmail.com>
2022-10-20 13:17:39 -0300

    Service layer, component registration

--- src/main/java/com/astro/apiwebService/resources/UserResource.java
+++ src/main/java/com/astro/apiwebService/resources/UserResource.java
@@ -1,21 +1,40 @@
 package com.astro.apiwebService.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.astro.apiwebService.entities.User;
+import com.astro.apiwebService.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Davi", "davi@gmail.com", "999999", "12345");
+	public ResponseEntity<List<User>> findAll(){
 		
-		return ResponseEntity.ok().body(u);
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
+		
 	}
 	
 }
