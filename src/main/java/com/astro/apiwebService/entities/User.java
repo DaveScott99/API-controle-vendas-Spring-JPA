0448e2d5a628e7e62119d4e4e524f48a6a105fe3
DaviSantos <davicalixto2077@gmail.com>
2022-10-20 12:45:01 -0300

    H2 database, test profile, JPA

--- src/main/java/com/astro/apiwebService/entities/User.java
+++ src/main/java/com/astro/apiwebService/entities/User.java
@@ -3,10 +3,20 @@ package com.astro.apiwebService.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
