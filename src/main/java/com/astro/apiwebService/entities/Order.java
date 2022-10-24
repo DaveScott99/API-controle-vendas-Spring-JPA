2559359cb17aa68a52d3425259c5eb178da1ee70
DaviSantos <davicalixto2077@gmail.com>
2022-10-24 11:41:08 -0300

    Subtotal & Total methods

--- src/main/java/com/astro/apiwebService/entities/Order.java
+++ src/main/java/com/astro/apiwebService/entities/Order.java
@@ -103,6 +103,18 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	public Double getTotal() {
+		
+		double sum = 0;
+		
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		
+		return sum;
+		
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
