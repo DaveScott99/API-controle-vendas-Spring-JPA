b4e78a0a7be8eed16756df3ef5c34d4161fc0725
DaviSantos <davicalixto2077@gmail.com>
2022-10-24 12:39:21 -0300

    Exception handling - findById

--- src/main/java/com/astro/apiwebService/services/UserService.java
+++ src/main/java/com/astro/apiwebService/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.astro.apiwebService.entities.User;
 import com.astro.apiwebService.respositories.UserRepository;
+import com.astro.apiwebService.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
