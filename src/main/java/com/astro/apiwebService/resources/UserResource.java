bca0a3e373ebd61c3beaf2abf5d4a08252c2d33a
DaviSantos <davicalixto2077@gmail.com>
2022-10-24 11:54:08 -0300

    User insert

--- src/main/java/com/astro/apiwebService/resources/UserResource.java
+++ src/main/java/com/astro/apiwebService/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.astro.apiwebService.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.astro.apiwebService.entities.User;
 import com.astro.apiwebService.services.UserService;
@@ -35,6 +39,17 @@ public class UserResource {
 		
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		
+		obj = service.insert(obj);
+		
+		// Linha de código para converter o objeto em URI.
+		URI uri	= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }
