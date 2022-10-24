06edf5e896311982ae6ebeadb9006450c085eb51
DaviSantos <davicalixto2077@gmail.com>
2022-10-24 11:32:12 -0300

    Payment, one-to-one association

--- src/main/java/com/astro/apiwebService/config/TestConfig.java
+++ src/main/java/com/astro/apiwebService/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.astro.apiwebService.entities.Category;
 import com.astro.apiwebService.entities.Order;
 import com.astro.apiwebService.entities.OrderItem;
+import com.astro.apiwebService.entities.Payment;
 import com.astro.apiwebService.entities.Product;
 import com.astro.apiwebService.entities.User;
 import com.astro.apiwebService.entities.enums.OrderStatus;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2022-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
